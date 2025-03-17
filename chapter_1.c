69b1cf44daca8bcefe2181c6101a4c2fa41340ad
rabbyhasanrzs <rabbyhasanrzs518@gmail.com>
2025-03-17 21:37:05 +0600

    Add Chapter 1

---
+++ chapter_1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    const int num = 23;   //here the exam of variable
+    char ch = 'R';   //here the exam of variable
+    float pi  = 3.1416;   //here the exam of variable
+
+    printf("The num is = %d ",num);
+    return 0;
+}
